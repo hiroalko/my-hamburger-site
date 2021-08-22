8207e97a088f004d02bf33bc31f12c796a4967f0
hiroaki <hiroe.com.yuma@icloud.com>
2021-08-22 10:01:44 +0900

    thirty-seven commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-
+$('.p-header__sp-title').on('click',function(){
+    $('.l-sidebar').toggleClass('active');
+    $('body').css('position', 'fixed');
+    $('.l-sidebar__overlay').toggleClass('active');
+});
+// 押したらサイドバーが現れる
+$('.batsu').click(function() {
+    $('.l-sidebar').toggleClass('active');
+    $('body').css('position', 'relative');
+    $('.l-sidebar__overlay').toggleClass('active');
+});
+// 押したらサイドバーが消える
        
